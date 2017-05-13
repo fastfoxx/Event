b0be08276333f2451e94c2adf7fa074c18bf897a
Firenzo <firenzo_akkefietje@msn.com>
2017-05-13 15:41:02 +0200

    qeweqqwe

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -174,14 +174,14 @@ function whichboss(nummer){
 
 	if(rest < 0.1){
 		if(active == "GMT"){return bossOrder[2]};
-		return bossOrder[1];
-	};
-	if(rest > 0.1 && rest < 0.4){
-		if(active == "GMT"){return bossOrder[1]};
 		return bossOrder[0];
 	};
-	if(rest > 0.5 && rest < 0.7){
+	if(rest > 0.1 && rest < 0.4){
 		if(active == "GMT"){return bossOrder[0]};
+		return bossOrder[1];
+	};
+	if(rest > 0.5 && rest < 0.7){
+		if(active == "GMT"){return bossOrder[1]};
 		return bossOrder[2];
 	};
 	//Aomak > Excavation > Croxar
