320e3bec2ef69187cb83ea98f6de1d24d74f5341
Firenzo <firenzo_akkefietje@msn.com>
2017-05-17 21:23:18 +0200

    aec

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -173,16 +173,16 @@ function whichboss(nummer){
 	var rest = checkfloat - checkint;
 
 	if(rest < 0.1){
-		if(active == "GMT"){return bossOrder[2]};
+		if(active == "GMT"){return bossOrder[0]};
 		return bossOrder[1];
 	};
 	if(rest > 0.1 && rest < 0.4){
 		if(active == "GMT"){return bossOrder[1]};
-		return bossOrder[0];
+		return bossOrder[2];
 	};
 	if(rest > 0.5 && rest < 0.7){
-		if(active == "GMT"){return bossOrder[0]};
-		return bossOrder[2];
+		if(active == "GMT"){return bossOrder[2]};
+		return bossOrder[0];
 	};
 	//Aomak > Excavation > Croxar
 	//bossOrder = ['Aomak','Excavation','Croxar'];
